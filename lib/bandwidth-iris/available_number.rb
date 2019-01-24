4f91237adb180d528cca5f8a6d4f5e908acf51bf
Nick Delja <nick@sevenwire.com>
2019-01-24 09:45:11 -0800

    allow enableTNDetail

--- lib/bandwidth-iris/available_number.rb
+++ lib/bandwidth-iris/available_number.rb
@@ -5,11 +5,12 @@ module BandwidthIris
     extend ClientWrapper
 
     def self.list(client, query)
-      list = client.make_request(:get, client.concat_account_path(AVAILABLE_NUMBER_PATH), query)[0][:telephone_number_list]
+      response = client.make_request(:get, client.concat_account_path(AVAILABLE_NUMBER_PATH), query)
+      list = response[0] && response[0][:telephone_number_list] && response[0][:telephone_number_list][:telephone_number]
+      list ||= response[0] && response[0][:telephone_number_detail_list] && response[0][:telephone_number_detail_list][:telephone_number_detail]
       return [] if !list
       if list.is_a?(Array) then list else [list] end
     end
     wrap_client_arg :list
-
   end
 end
