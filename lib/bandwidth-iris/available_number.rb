3f20dc3dcb135375052a43641d24f554ffefe723
Nick Delja <nick@sevenwire.com>
2019-01-23 20:32:10 -0800

    Update available_number.rb

--- lib/bandwidth-iris/available_number.rb
+++ lib/bandwidth-iris/available_number.rb
@@ -5,7 +5,7 @@ module BandwidthIris
     extend ClientWrapper
 
     def self.list(client, query)
-      list = client.make_request(:get, client.concat_account_path(AVAILABLE_NUMBER_PATH), query)[0][:telephone_number_list][:telephone_number]
+      list = client.make_request(:get, client.concat_account_path(AVAILABLE_NUMBER_PATH), query)[0][:telephone_number_list]
       return [] if !list
       if list.is_a?(Array) then list else [list] end
     end
